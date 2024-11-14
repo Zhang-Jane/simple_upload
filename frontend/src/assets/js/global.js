b236364174b75962bfb694f5846a6ee992c00959
zhangjian <zhangjian@zingfront.com>
2024-11-14 10:41:20 +0800

    fix: 跨域错误

--- frontend/src/assets/js/global.js
+++ frontend/src/assets/js/global.js
@@ -6,5 +6,5 @@
 export const IS_DEV = false;
 
 export const DEVELOPMENT = "http://127.0.0.1:9011/api/v1"; // 开发环境
-export const PRODUCTION = "http://127.0.0.1:9011/api/v1"; // 线上环境
+export const PRODUCTION = "http://192.168.100.10:80/api/v1"; // 线上环境
 export const BACKEND_BASE_URL = IS_DEV ? DEVELOPMENT : PRODUCTION;
